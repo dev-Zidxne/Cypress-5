5d28b47b5f183549471dc0f6f7682b2a1b18d019
Zidane Innis <zidaneinnis@gmail.com>
2023-06-19 19:56:29 -0500

    Understanding & Using Interceptors

--- cypress/e2e/newsletter.cy.js
+++ cypress/e2e/newsletter.cy.js
@@ -1,13 +1,15 @@
 /// <reference types="Cypress" />
 
-describe('newletter signup', () => {
+describe('Newsletter', () => {
 	beforeEach(() => {
 		cy.task('seedDatabase');
 	});
-	it('should signup user to newlsetter', () => {
+	it('should display a success message', () => {
+		cy.intercept('POST', '/newsletter*', { status: 201 }).as('subscribe'); //intercept any HTTP request send to localhost:300/newsletter
 		cy.visit('/');
 		cy.get('[data-cy="newsletter-email"]').type('test@example.com');
 		cy.get('[data-cy="newsletter-submit"]').click();
+		cy.wait('@subscribe');
 		cy.contains('Thanks for signing up');
 	});
 });
